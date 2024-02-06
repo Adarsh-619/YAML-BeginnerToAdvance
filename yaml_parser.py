16fc6637a6f80d48b092209ca2a627cf184df5a8
Adarsh Patra <patra.adarsh9@gmail.com>
2024-02-07 02:49:55 +0530

    Fixed the linting issues and modified advancedDatatype

--- yaml_parser.py
+++ yaml_parser.py
@@ -1,14 +1,25 @@
-import yaml
+"""
+This module provides functionality to parse the yaml to json.
+
+It makes use of the yaml module to process the yaml file and converts it into json. 
+The repository currently has 3 files which covers all the basics of YAML
+
+Author: Adarsh Patra
+Date: February 6, 2024
+"""
+
+
 import json
-import pprint
+# import pprint
 import os
+import yaml
 from termcolor import colored
 
 
 files = [f for f in os.listdir(os.curdir) if os.path.isfile(f)
-          and (f.endswith('yaml') or f.endswith('yaml'))]
+         and (f.endswith('yaml') or f.endswith('yaml'))]
 
-fileName = ""
+FILE_NAME = ""
 
 print("Please select the file:")
 for i, f in enumerate(files):
@@ -16,14 +27,14 @@ for i, f in enumerate(files):
 
 selectedFile = int(input())
 try:
-    fileName = files[selectedFile - 1] 
-except IndexError:
-    raise Exception("Invalid input. Please try again")
+    FILE_NAME = files[selectedFile - 1]
+except IndexError as exc:
+    raise IndexError('Invalid input. Please try again') from exc
 
 print("**************")
 
-with open(fileName, 'r') as f:
-	# data = yaml.load(f, Loader=yaml.SafeLoader)
+with open(FILE_NAME, 'r', encoding='utf-8') as f:
+    # data = yaml.load(f, Loader=yaml.SafeLoader)
     try:
         # data = yaml.safe_load(f) # For single document
         data = yaml.safe_load_all(f)
@@ -34,20 +45,18 @@ with open(fileName, 'r') as f:
 
         # print("Printing the multiline:\n" + data['multiline'])
 
-
         # Multiple Document
-        multilineDoc = ""
+        MULTILINE_DOC = ""
         for ind, item in enumerate(data):
-            if (fileName == "yaml_basics.yaml" and ind == 1):
-                multilineDoc = item['multiline']
+            if (FILE_NAME == "yaml_basics.yaml" and ind == 1):
+                MULTILINE_DOC = item['multiline']
             print(f'Document - {ind + 1}\n**************')
             print(json.dumps(item, indent=4))
             print('\n**************\n')
 
-        if fileName == "yaml_basics.yaml":
-            print("Printing the multiline:\n" + multilineDoc)
+        if FILE_NAME == "yaml_basics.yaml":
+            print("Printing the multiline:\n" + MULTILINE_DOC)
     except yaml.YAMLError as exc:
         print(exc)
     except TypeError as exc:
         print(colored(exc, 'red'))
-        
